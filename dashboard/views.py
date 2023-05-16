1cb39f1ae454138fd9672ea2fcde3aa81f84d6ba
MihaiElisei <>
2023-05-16 20:27:25 +0000

    added dashboard templates css and urls

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def dashboard(request):
+    """ A view to return the dashboard page """
+    
+    return render(request, 'dashboard/dashboard.html')
